9e8145d67170b9150f531c43b422114e3959e730
Al Miller <almiller@Als-MacBook-Pro.local>
2023-03-22 17:21:15 -0400

    new files + gitignore

--- hello_world/main.cpp
+++ hello_world/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream> 
+using namespace std;
 
 int main() {
-    std::cout << "hello mac command line" << std::endl;
+    // std::cout << "hello mac command line" << std::endl;
+
+    // cout << "Hello World!"; // does not add new line
+
+    cout << "hello " << " world" << endl; // can chain together 
+    
+    cout << "hello " << " world \n"; // 
+
+    cout << "hello\ncpp\nworld";
+
     return 0;
 }
 
