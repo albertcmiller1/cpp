cf1fe8e4a25d806d7081079f527d0eca951649db
Al Miller <almiller@Als-MacBook-Pro.lan>
2023-03-22 22:02:08 -0400

    moved dirs

---
+++ variables/cin_cout.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num; 
+    std::cout << "enter your fav number between 1 and 100: ";
+    std::cin >> num ; 
+    std::cout << "Amazing! " << num << " is my fav number too. " << std::endl;
+    return 0;
+}
---
+++ variables/local_global.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+using namespace std; 
+
+int age(18); // global variable 
+int age2 = 19; 
+
+int main(){
+    // int age(16); // local variable will overwrite s
+    cout << age << endl;
+    cout << age2 << endl;
+    return 0;
+}
+
+
