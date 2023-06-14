d370355b45fbd933e132449c4f513984cfe09308
albertcmiller1 <albertcmiller1@gmail.com>
2023-06-14 14:37:38 -0400

    arrays

---
+++ arrays_vectors/8.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <vector> 
+using namespace std;
+
+vector<int> doit(vector<int> *v, int new_val){
+
+    for (int i=0; i<v->size(); i++){
+        if (new_val > v->at(i)){
+            v->insert(v->begin() + i, new_val);
+        }
+    }
+
+    // for (auto i : *v){
+    //     if (new_val > i){
+    //     }
+    // }
+    return *v;
+}
+
+
+int main()
+{
+    vector<int> v {NULL};
+
+
+    doit(&v, 50);
+    doit(&v, 10);
+    doit(&v, 20);
+    doit(&v, 40);
+    doit(&v, 30);
+    // v = doit(v);
+
+
+
+
+    // print
+    for (auto i : v){
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
--- pointers_references/15.cpp
+++ pointers_references/15.cpp
@@ -3,11 +3,16 @@ using namespace std;
 
 void display(vector<string> *v){
     (*v).at(0) = "poopy";
-    
     for (auto str : *v)
         cout << str << " ";
     cout << endl;
+}
 
+
+void display_other(int *arr, int sentinel){
+    while (*arr != sentinel)
+        cout << *arr++ << " ";
+    cout << endl;
 }
 
 int main(){
@@ -15,6 +20,9 @@ int main(){
     vector<string> ppl {"mo", "chris", "albert"};
     display(&ppl);
 
+
+    int scores[] {100, 200, 300, -1};
+    display_other(scores, -1);
 
     return 0;
 }
