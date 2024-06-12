37f3b883202131a70d05c90304c7e70ec53e92fc
albertcmiller1 <albertcmiller1@gmail.com>
2024-06-12 11:44:32 -0400

    more leetcode

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
- 
 
 // Vectors
 #include <vector>
@@ -14,6 +13,8 @@ void vectors_test(){
     for (int i=0; i<v3.size(); i++){
         std::cout << v3[i] << std::endl;
     }
+
+    bool is_empty = v1.empty();
 
     int end = v2.back();
     v2.pop_back();
@@ -60,17 +61,60 @@ void map_test(){
 }
 
 
-
-
-
-
 // Queues 
+#include <queue>
+void queue_test(){
+    // if we want to push and pop from both sides of the queue, need to use deque (double ended queue) instead. 
+    std::queue<int> q1;
+    std::deque<int> q2;
+
+    q2.push_front(1);
+    q2.push_back(2);
+    int back = q2.back();
+    int front = q2.front();
+    q2.pop_back();
+    q2.pop_front();
+
+
+
+    q1.push(66); // push to back of queue
+    q1.push(77);
+
+    int front2 = q1.front();
+    int back2 = q1.back();
+
+    q1.pop(); // pops from left side of queue 
+
+}
+
+
+// Heaps 
+// priority queue is included in the queue package. 
+void test_heap(){
+    // Max heap by default 
+    std::priority_queue<int> heap1; // cant initialize 
+    std::vector<int> arr {1, 2, 3};
+    std::priority_queue<int> heap2 (arr.begin(), arr.end());
+
+    heap1.push(11);
+    heap1.push(22);
+    heap1.push(33);
+
+    int top = heap1.top();
+    heap1.pop();
+
+    bool is_empty = heap1.empty();
+
+    int size = heap2.size();
+}
 
 
 
 // Main Entry 
 int main(){
     vectors_test();
+    queue_test();
+    test_heap();
     // set_test();
     // map_test();
     return 0;
